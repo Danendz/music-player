6519c5ec041593c9dcae3562221eee41a42cee82
Danendz <danilapayne@gmail.com>
2020-05-10 10:05:21 +0500

    Try to do notification navigator

--- components/js/app.js
+++ components/js/app.js
@@ -240,31 +240,29 @@ mute.addEventListener('click', () => {
 });
 
 if ('mediaSession' in navigator) {
-
     navigator.mediaSession.metadata = new MediaMetadata({
-      title: 'Never Gonna Give You Up',
-      artist: 'Rick Astley',
-      album: 'Whenever You Need Somebody',
-      artwork: [
-        { src: 'https://dummyimage.com/96x96',   sizes: '96x96',   type: 'image/png' },
-        { src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
-        { src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
-        { src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
-        { src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
-        { src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' },
-      ]
+        title: music[activeMusic].name,
+        artist: music[activeMusic].author,
+        artwork: [
+            { src: music[activeMusic].src, sizes: '96x96', type: 'image/jpg' },
+            { src: music[activeMusic].src, sizes: '128x128', type: 'image/jpg' },
+            { src: music[activeMusic].src, sizes: '192x192', type: 'image/jpg' },
+            { src: music[activeMusic].src, sizes: '256x256', type: 'image/jpg' },
+            { src: music[activeMusic].src, sizes: '384x384', type: 'image/jpg' },
+            { src: music[activeMusic].src, sizes: '512x512', type: 'image/jpg' },
+        ]
     });
-  
-    navigator.mediaSession.setActionHandler('seekbackward', function() {
+
+    navigator.mediaSession.setActionHandler('seekbackward', function () {
         myAudio.currentTime -= 5;
     });
-    navigator.mediaSession.setActionHandler('seekforward', function() {
+    navigator.mediaSession.setActionHandler('seekforward', function () {
         myAudio.currentTime += 5;
     });
-    navigator.mediaSession.setActionHandler('previoustrack', function() {
+    navigator.mediaSession.setActionHandler('previoustrack', function () {
         previousSong();
     });
-    navigator.mediaSession.setActionHandler('nexttrack', function() {
+    navigator.mediaSession.setActionHandler('nexttrack', function () {
         nextSong();
     });
-  }
+}
